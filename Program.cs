8954c7c86564b61bc5cccf50d59b04b86d9bc78e
Oleg140 <79318243+Oleg140@users.noreply.github.com>
2021-03-19 19:13:24 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,25 +1,27 @@
 ﻿using System;
-
-namespace zaglavnbukv
+namespace Slowa
 {
     class Program
     {
-        static void FindNonEnglish(String s)
-        {
-            foreach (char c in s)
-            {
-                if (char.IsUpper(c) && (c > 90))
-                {
-                    Console.WriteLine(c);
-                }
-            }
-        }
         static void Main(string[] args)
         {
-            String s;
-            Console.WriteLine("Enter the line:");
-            s = Console.ReadLine();
-            FindNonEnglish(s);
+            string d = Console.ReadLine();
+
+            string[] words = d.Split(' ');
+
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                Console.Write("{0}", words[i]);
+            }
+            Array.Reverse(words);// слова в words в обратном порядке
+
+            Console.WriteLine("reversed:");
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.Write(words[i] + " ");
+            }
+            Console.ReadKey();
         }
     }
 }

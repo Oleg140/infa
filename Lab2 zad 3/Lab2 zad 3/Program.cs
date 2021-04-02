33bc270aead1ad20046d682f054ab93207995931
Oleg140 <79318243+Oleg140@users.noreply.github.com>
2021-04-02 19:48:36 +0300

    Add files via upload

---
+++ Lab2 zad 3/Lab2 zad 3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Permutations
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите строку:");
+
+            var str = Console.ReadLine();
+
+            var comb = new Combinatorics();
+            var numb = 0;
+
+            Console.WriteLine("\nПерестановки:\n");
+
+            foreach (var permutation in comb.Permutation(str))
+            {
+                Console.WriteLine(permutation);
+                numb++;
+            }
+
+            Console.WriteLine($"\nОбщее число перестановок: {numb}");
+        }
+    }
+
+    public class Combinatorics
+    {
+        public IEnumerable<string> Permutation(string s)
+        {
+            if (s == string.Empty)
+                yield return string.Empty;
+            else
+            {
+                var set = new SortedSet<char>(s);
+
+                foreach (var ch in set)
+                {
+                    var substring = (new StringBuilder(s).Remove(s.IndexOf(ch), 1)).ToString();
+                    foreach (var p in Permutation(substring))
+                        yield return ch + p;
+                }
+            }
+        }
+    }
+}

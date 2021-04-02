d8a286306d2de1a2b22db5131372cfc58b6388f3
Oleg140 <79318243+Oleg140@users.noreply.github.com>
2021-04-02 19:23:12 +0300

    Add files via upload

---
+++ Lab 2zad 1/Lab2zad1/Lab2zad1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab2zad1
+{ class program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            string d = Console.ReadLine();
+
+            string[] words = d.Split(' ');
+
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                Console.Write("{0}", words[i]);
+            }
+            Array.Reverse(words);// слова в words в обратном порядке
+
+            Console.WriteLine("reversed:");
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.Write(words[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+

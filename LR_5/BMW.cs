6b128ee9036fec9691363955e98db586f8c2f9f8
Oleg140 <79318243+Oleg140@users.noreply.github.com>
2021-06-04 20:39:54 +0300

    Add files via upload

---
+++ LR_5/BMW.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LR_5
+{
+    class BMW : Car
+    {
+        public BMW()
+        {
+            Mk = "BMW";
+            MaxAcceleration = 5;
+            MaxBraking = 2;
+            MaxSpeed = 40;
+        }
+
+        public override void Beep()
+        {
+            Console.WriteLine("Би-би-бип. Вы бибикнули.");
+        }
+    }
+}
---
+++ LR_5/Volkswagen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LR_5
+{
+    class Volkswagen : Car
+    {
+        public Volkswagen()
+        {
+            MaxSpeed = 35;
+            MaxAcceleration = 3;
+            MaxBraking = 2;
+            Mk = "Volkswagen";
+        }
+
+        public override void Beep()
+        {
+            Console.WriteLine("Би-бииип. Вы бибикнули.");
+        }
+    }
+}
